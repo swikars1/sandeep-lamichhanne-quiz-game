6713443c14afc0cbbbe0776f05607cd25bc71a80
swikars1 <swikarsharma@gmail.com>
2019-03-07 17:23:21 +0545

    to doss

--- js/main.js
+++ js/main.js
@@ -1,33 +1,30 @@
-$(document).ready(function(){
-    const requestURL = './lib/qna.json'; //questions JSON   
-    var questionCounter = 0;
+$(document).ready(function() {
+  const requestURL = "./lib/qna.json"; //questions JSON
+  var questionCounter = 0;
+  var questionDatum;
 
-    switch(questionCounter){
-        case 0:
+  function getFromJson(requestURL, callback) {
+    $.getJSON(requestURL, function(questionData) {
+        callback(questionData);
+    });
+  }
 
-        break;
-        }
-
-    function getFromJson(requestURL, quesNum){
-        $.getJSON(requestURL, function(questionData){
-            populateQuestion(questionData, quesNum);
-        });
-    }
-
-    function populateQuestion(questionData, n){
-        let loadingQuestion = questionData.questions[n].question; 
-        $('#scoreboard').after(`<p class = "questionClass">${loadingQuestion}</p>`);
-        populateOption(questionData, n);
-    }
-
-    function populateOption(questionData, n){
-        option1 = questionData.questions[n].option1;
-        option2 = questionData.questions[n].option2;
-        option3 = questionData.questions[n].option3;
-        option4 = questionData.questions[n].option4;
-        $('.options').append(`<li>${option1}</li>`);
-        $('.options').append(`<li>${option2}</li>`);
-        $('.options').append(`<li>${option3}</li>`);
-        $('.options').append(`<li>${option4}</li>`);
-    }
+  function populateQuestion(questionData, n) {
+    let loadingQuestion = questionData.questions[n].question;
+    $("#scoreboard").after(`<p class = "questionClass">${loadingQuestion}</p>`);
+    populateOption(questionData, n);
+  }
+  
+  function populateOption(questionData, n) {
+    option1 = questionData.questions[n].option1;
+    $(".options").append(`<li>${option1}</li>`);
+  }
+  getFromJson(requestURL, populateQuestion(questionData));
 });
+
+//TODO:
+// model to load question
+// model to load options    
+// answer checker
+// slide player
+// next button system
