66787e82501533e1143fff4b9bad2e05ebf3a3c0
swikars1 <swikarsharma@gmail.com>
2019-03-12 11:05:38 +0545

    fb connection in progress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
   const requestURL = "./lib/qna.json"; //questions JSON
@@ -40,3 +41,53 @@ $(document).ready(function() {
 // answer checker
 // slide player
 // next button system
+
+// initialize and setup facebook js sdk
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '315495869163777',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        document.getElementById('status').innerHTML = 'We are connected.';
+        document.getElementById('login').style.visibility = 'hidden';
+      } else if (response.status === 'not_authorized') {
+        document.getElementById('status').innerHTML = 'We are not logged in.'
+      } else {
+        document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+      }
+    });
+};
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// login with facebook with extra permissions
+$('#button').on('click', function login() {
+  FB.login(function(response) {
+    if (response.status === 'connected') {
+        document.getElementById('status').innerHTML = 'We are connected.';
+        document.getElementById('login').style.visibility = 'hidden';
+      } else if (response.status === 'not_authorized') {
+        document.getElementById('status').innerHTML = 'We are not logged in.'
+      } else {
+        document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+      }
+  }, {scope: 'email'});
+});
+
+$('#button').on('click',function getInfo() {
+FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
+  var userId = document.getElementById('status').innerHTML = response.id;
+});
+});
+
+$("#button").click(function(){
+  $('.hidden').hide();
+});
